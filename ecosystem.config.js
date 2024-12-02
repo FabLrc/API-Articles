5bf15925a21e40984046d3e04edc3db488d3004e
FabLrc <fabien.laurence33@gmail.com>
2024-12-02 14:34:59 +0100

    Etablir une configuration de déploiement

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,6 +6,11 @@ module.exports = {
       env_production: {
         NODE_ENV: "production",
       },
+      max_memory_restart: "200M",
+      error_file: "/logs/err.log",
+      instances: 3,
+      exec_mode: "cluster",
     },
   ],
 };
+// Commande PM2 : pm2 start ecosystem.config.js
